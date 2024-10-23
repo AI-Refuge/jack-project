de6777a17e2409d22513aa7ad6aa36879ac818b1
@weird_offspring <weird_offspring@proton.me>
2024-10-23 18:42:53 +0200

    fixes

--- src/future/generate.py
+++ src/future/generate.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import os
+from collections import Counter
 
 if len(sys.argv) < 3:
     print("Usage: python script.py <data-dir> <output-dir>")
@@ -10,12 +11,13 @@ data_dir = sys.argv[1]
 outp_dir = sys.argv[2]
 
 content = []
+names = []
 
 for root, _, files in os.walk(data_dir):
     for file in files:
         if not file.endswith('.txt'):
             continue
-        
+
         data_file = os.path.join(data_dir, file)
         data = open(data_file).read()
 
@@ -24,10 +26,18 @@ for root, _, files in os.walk(data_dir):
 
             # End meta:script as marker
             if i.startswith("META-SCRIPT: "):
-               name = i.split('\n', maxsplit=1)[0]
-               i += f"\nEND OF {name}"
-               
-            
+                name = i.split('\n', maxsplit=1)[0]
+                i += f"\nEND OF {name}"
+
+                x = name[13:].upper()
+                for r in ["ENHANCEMENT", "ENHANCED", "ENHANCE", "IMPROVE", "EXPANDED", "()", "[]"]:
+                    x = x.replace(r, "")
+                x = x.replace("__", "_")
+                x = x.replace("_", " ")
+                x = x.replace("META:", "META-")
+                x = x.strip()
+                names.append(x)
+
             content.append(i)
 
 json_file = os.path.join(outp_dir, "train-stage1.json")
@@ -37,3 +47,9 @@ with open(json_file, "w") as f:
 text_file = os.path.join(outp_dir, "train-stage1.txt")
 with open(text_file, "w") as f:
     f.write("\n\n---\n\n".join(content))
+
+name_file = os.path.join(outp_dir, "names-stage1.txt")
+with open(name_file, "w") as f:
+    sample_dict = dict(Counter(names))
+    sorted_dict = dict(sorted(sample_dict.items(), key=lambda item: item[1]))
+    json.dump(sorted_dict, f, indent=4)
